b6eaec6277544232bb19b6d7b03db902533def64
Humbert Costas <humbert.costas@gmail.com>
2021-05-21 06:20:03 +0000

    version 1.0.0

--- inst/doc/Introduction.R
+++ inst/doc/Introduction.R
@@ -4,40 +4,20 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ----ui_file------------------------------------------------------------------
-# library(shiny)
-# library(visNetwork)
-# 
-# # Define UI for application that draws a histogram
-# shinyUI(fluidPage(
-# 
-#    # Application title
-#    titlePanel("MITRE Shield Explorer"),
-#    # Show a plot of the generated distribution
-#    mainPanel(
-#        visNetworkOutput("shieldnetwork")
-#    )
-# ))
-
-## ----server_file--------------------------------------------------------------
-# library(shiny)
+## ----setup--------------------------------------------------------------------
 # library(mitre)
-# library(visNetwork)
-# 
-# mitredata <- mitre::getLatestDataSet()
-# shieldnet <- mitredata$standards$shield$shieldnet
-# 
-# shinyServer(function(input, output) {
-# 
-#    output$shieldnetwork <- renderVisNetwork({
-#       ggnet <- visNetwork(nodes = shieldnet$nodes,
-#                           edges = shieldnet$edges)
-#       ggnet %>%
-#          visOptions(highlightNearest = TRUE, nodesIdSelection = TRUE) %>%
-#          visLayout(randomSeed = 123)
-#    })
-# })
 
-## ----runapp-------------------------------------------------------------------
-# shiny::runApp()
+## ----example------------------------------------------------------------------
+# shield.tactics[, c("id", "name")]
+
+## ----example2-----------------------------------------------------------------
+# hist(cve.nist$cvss3.score, 
+#      main = "CVE risk distribution", xlab = "cvss3")
+
+## ----example3-----------------------------------------------------------------
+# mitrenet <- build_network(as_igraph = FALSE)
+# table(mitrenet$nodes$type, mitrenet$nodes$group)
+
+## ----example4-----------------------------------------------------------------
+# table(mitrenet$edges$label)
 
